12c855f04d3a290235d545c02faccefb57ce2c67
faresasmer <90081204+faresasmer@users.noreply.github.com>
2023-08-14 14:09:48 +0300

    Update faris.tsx

--- pages/faris.tsx
+++ pages/faris.tsx
@@ -6,8 +6,7 @@ export default function Faris() {
 
         <h1>hello faris pages</h1>
         <h1>hello faris pages</h1>
-<h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1>
-
+        <h1> staering thew alst of the wlat eof thew marking in the last of my company </h1>
         <Link href={'./'}>back home</Link>
         </>
     )
