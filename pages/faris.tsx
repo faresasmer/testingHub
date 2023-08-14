47be2f3019c7bd315342809ab4d6f6af2ea92edf
fares <fares.asmer5s@gmail.com>
2023-08-14 13:47:47 +0300

    Update faris.tsx

--- pages/faris.tsx
+++ pages/faris.tsx
@@ -3,13 +3,11 @@ import Link from "next/link";
 export default function Faris() {
     return (
         <>
-<<<<<<< HEAD
-=======
+
         <h1>hello faris pages</h1>
         <h1>hello faris pages</h1>
 <h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1>
 
->>>>>>> faresTest
         <Link href={'./'}>back home</Link>
         </>
     )
