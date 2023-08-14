e90fc4b834ebd1b57199d6bac8821211eabfec27
fares <fares.asmer5s@gmail.com>
2023-08-14 13:14:25 +0300

    updated fares.tsx files in pages

--- pages/faris.tsx
+++ pages/faris.tsx
@@ -4,7 +4,8 @@ export default function Faris() {
     return (
         <>
         <h1>hello faris pages</h1>
-        <h1>hello faris pages</h1>
+        <h1>hello faris pages</h1>\
+<h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1><h1>hello faris pages</h1>
 
         <Link href={'./'}>back home</Link>
         </>
